46e4ec8bfc227e94aadac862164139dd5cde8f7e
Marianne Porter <38002984+marianneporter@users.noreply.github.com>
2024-01-24 16:16:58 +0000

    heroku changes

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ]
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss()               
+            ],
+        },
+    },
 });
